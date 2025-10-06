1eb1eee60205e5a1135bbb4a06e950a9a84e3ec6
TobyJohnstonUU <johnston-t23@ulster.ac.uk>
2025-10-06 15:12:41 +0100

    Updated code with access modifiers

---
+++ Library/Book.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Library
+{
+    internal class Book
+    {
+        String Title;
+        String Author;
+        String ISBN;
+
+
+        public Book(string title, string author, string iSBN)
+        {
+            Title = title;
+            Author = author;
+            ISBN = iSBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,24 +1,7 @@
-﻿class Book
+﻿using Library;
+
+class Program
 {
-    String Title;
-    String Author;
-    String ISBN;
-
-
-    public Book(string title, string author, string iSBN)
-    {
-        Title = title;
-        Author = author;
-        ISBN = iSBN;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-    }
-
     static void Main(String[] args)
     {
         Book book = new Book("C# for beginners", "BillGates", "12345678");
@@ -26,3 +9,4 @@
         book.DisplayInfo();
     }
 }
+    
