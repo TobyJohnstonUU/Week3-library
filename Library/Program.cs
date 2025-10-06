14681675150acd7c9620a9d05b748f35568d2719
TobyJohnstonUU <johnston-t23@ulster.ac.uk>
2025-10-06 15:01:20 +0100

    Created new classes

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    String Title;
+    String Author;
+    String ISBN;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book Title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+    }
+
+    static void Main(String[] args)
+    {
+        Book book = new Book();
+
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        Book book1 = new Book();
+
+        book1.Title = "C# Methods and Classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "87654321";
+
+        book.DisplayInfo();
+        book1.DisplayInfo();
+    }
+}
